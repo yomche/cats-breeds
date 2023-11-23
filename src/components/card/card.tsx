cfdeababda1865d23b997edc50c87c4e25f8f19b
Ekaterina Dorozhkina <katerina.smirnova98@yandex.ru>
2023-11-23 19:00:40 +0300

    Add package

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,4 +1,7 @@
+import Flag from "react-world-flags";
 import { CatBreedType } from "../../types/types";
+import { CountriesMap } from "../../utils/countries";
+import Unknown from "../../assets/Unknown_flag.png";
 import styles from "./card.module.scss";
 
 interface CardProps {
@@ -8,11 +11,38 @@ interface CardProps {
 export const Card = ({ data }: CardProps) => {
   return (
     <div className={styles.card}>
-      <div className={styles.header}>{data.breed}</div>
-      <div>{data.country}</div>
-      <div>{data.origin}</div>
-      <div>{data.coat}</div>
-      <div>{data.pattern}</div>
+      <div className={styles.header}>
+        <a
+          href={`https://www.google.com/search?q=${data.breed}+сat&newwindow=1`}
+          target="_blank"
+        >
+          {data.breed}
+        </a>
+        <Flag
+          className={styles.flag}
+          code={CountriesMap[data.country]}
+          height="20"
+          fallback={<img src={Unknown} height={20} width={40} />}
+        />
+      </div>
+      {data.origin && (
+        <div className={styles.item}>
+          <p>Origin: </p>
+          {data.origin}
+        </div>
+      )}
+      {data.coat && (
+        <div className={styles.item}>
+          <p>Coat: </p>
+          {data.coat}
+        </div>
+      )}
+      {data.pattern && (
+        <div className={styles.item}>
+          <p>Pattern: </p>
+          {data.pattern}
+        </div>
+      )}
     </div>
   );
 };
