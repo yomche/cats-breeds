e247dd1c442b4eeec1b97593979680c6c1722370
Ekaterina Dorozhkina <katerina.smirnova98@yandex.ru>
2023-11-23 19:44:33 +0300

    Fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/cats-breeds/",
+  envDir: "./env",
+  server: {
+    host: "127.0.0.1",
+    port: 3000,
+  },
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+  },
+});
